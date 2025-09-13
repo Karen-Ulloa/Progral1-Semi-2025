2415a474c77ad0ff8f6ea7a43ddf6bb2118fc5f4
Ulloa <Ulloa@LAPTOP-C4A4PTCQ>
2025-09-12 21:07:17 -0700

    Creación de la BD y la clase conexion

--- MiPrimerProyectoCsharp/MiPrimerProyectoCsharp/Program.cs
+++ MiPrimerProyectoCsharp/MiPrimerProyectoCsharp/Program.cs
@@ -17,7 +17,7 @@ namespace MiPrimerProyectoCsharp
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new ConversorConMatriz());
+            Application.Run(new Form3());
         }
     }
 }
